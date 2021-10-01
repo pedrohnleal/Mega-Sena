cf7a0b54c36a6114e9caf1502c9a5c5a92f87c18
Pedro Henrique <phnl@outlook.com>
2021-10-01 15:21:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace megaSena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            megasena(1, 60);
+        }
+        static void megasena(int min, int max)
+        {
+            Random num = new Random();
+            for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(num.Next(min, max));
+            }
+            Console.ReadLine();
+        }
+    }
+}
